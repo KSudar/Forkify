d00a5198e7fd6c43d9e33387a9a569c547298888
Kresimir Sudar <kresimir.sudar@ericsson.com>
2019-02-13 16:12:23 +0100

    Section 9, Lecture 150

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -61,13 +61,14 @@ export const rederRecipe = recipe => {
                 </svg>
                 <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
                 <span class="recipe__info-text"> servings</span>
+                
                 <div class="recipe__info-buttons">
-                    <button class="btn-tiny">
+                    <button class="btn-tiny btn-decrease">
                         <svg>
                             <use href="img/icons.svg#icon-circle-with-minus"></use>
                         </svg>
                     </button>
-                    <button class="btn-tiny">
+                    <button class="btn-tiny btn-increase">
                         <svg>
                             <use href="img/icons.svg#icon-circle-with-plus"></use>
                         </svg>
@@ -109,4 +110,15 @@ export const rederRecipe = recipe => {
         </div>
   `;
     elements.recipe.insertAdjacentHTML('afterbegin', markup);
+};
+
+export const updateServingsIngredients = recipe => {
+  // Update servings
+  document.querySelector('.recipe__info-data--people').textContent = recipe.servings
+  
+  // Update ingredients
+  const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+  countElements.forEach((el, i) => {
+     el.textContent =  formatCount(recipe.ingredients[i].count);
+  });
 };
